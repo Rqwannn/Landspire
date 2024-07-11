7144c5fb3183be1273e8737c4bbf5aad771aa74a
Rqwannn <mohwannn1@gmail.com>
2024-07-12 02:08:12 +0700

    build ipfs pinata function & export variabel constant (client)

--- landspire_v1/src/app/libs/constant.js
+++ landspire_v1/src/app/libs/constant.js
@@ -1,3 +1,4 @@
-import koinLandNfts from "./KoinLandNfts.json"
-const contractAbi = koinLandNfts.abi
-const contractAddress = "0x5b61b54c817a12e1018f8Ad6417097F412A7CFac"
+import koinLandNfts from "@/app/lib/koinLandNfts"
+
+export const contractAbi = koinLandNfts.abi
+export const contractAddress = "0x5b61b54c817a12e1018f8Ad6417097F412A7CFac"
