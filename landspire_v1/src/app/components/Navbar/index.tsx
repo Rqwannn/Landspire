dee83b03c7d4a938cbd21656fbf7475ed6133a29
faizallmaullana <faizalmaulanasekias@gmail.com>
2024-07-11 23:40:11 +0700

    feat: navbar

--- landspire_v1/src/app/components/Navbar/index.tsx
+++ landspire_v1/src/app/components/Navbar/index.tsx
@@ -2,17 +2,18 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <nav className="bg-landspire_blue flex justify-between px-4 py-5 text-landspire_white">
-            <div className="title text-xl">
-                Landspire
+        <nav className="bg-landspire_blue flex justify-between px-[72px] py-4 text-landspire_white">
+            <div className="flex flex-row gap-3">
+                <img src="/assets/logos/logo.png" alt="lanspire" width={32} height={32} />
+                <p className="title italic text-xl ">Landspire</p>
             </div>
             <div className="list_menu">
                 <ul className="flex gap-7 px-3">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/">Wallet</Link></li>
-                    <li><Link href="/">Service</Link></li>
-                    <li><Link href="/">Contact</Link></li>
-                    <li><Link href="/">Login</Link></li>
+                    <li className="hover:bg-[#424ab6] py-1 px-2 rounded-lg duration-300"><Link href="/">Home</Link></li>
+                    <li className="hover:bg-[#424ab6] py-1 px-2 rounded-lg duration-300"><Link href="/">Wallet</Link></li>
+                    <li className="hover:bg-[#424ab6] py-1 px-2 rounded-lg duration-300"><Link href="/">Service</Link></li>
+                    <li className="hover:bg-[#424ab6] py-1 px-2 rounded-lg duration-300"><Link href="/">Contact</Link></li>
+                    <li className="hover:bg-[#424ab6] py-1 px-2 rounded-lg duration-300"><Link href="/">Login</Link></li>
                 </ul>
             </div>
         </nav>
